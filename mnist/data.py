4389b55f8c47b7c4bb124a8b04582794991a2fce
Shikhar Gupta <shikhar.gupta@utexas.edu>
2023-11-25 23:35:06 -0600

    removed multithreaded processing on dataset and implemented algo 1 for balcan

--- mnist/data.py
+++ mnist/data.py
@@ -2,20 +2,29 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision import transforms
 
+
 class VisionDataset:
     def __init__(self, name, root, download=True):
         self.root = root
         self.download = download
 
-        if name.lower() == 'mnist':
+        if name.lower() == "mnist":
             self.dataset_builder = self.get_mnist_dataset
         else:
-            raise NotImplementedError('Dataset {} not implemented'.format(name))
+            raise NotImplementedError("Dataset {} not implemented".format(name))
 
-    def get_dataloaders(self, batch_size=32, num_workers=4):
+    def get_dataloaders(self, batch_size=32, num_workers=1):
         train_dataset, test_dataset = self.dataset_builder()
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+        )
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+        )
         return train_loader, test_loader
 
     def get_mnist_dataset(self):
@@ -23,20 +32,24 @@ class VisionDataset:
             self.root,
             train=True,
             download=self.download,
-            transform = transforms.Compose([
-                transforms.ToTensor(),
-                #transforms.Normalize((0.1307,), (0.3081,))
-            ]),
+            transform=transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    # transforms.Normalize((0.1307,), (0.3081,))
+                ]
+            ),
         )
 
         test_dataset = MNIST(
             self.root,
             train=False,
             download=self.download,
-            transform = transforms.Compose([
-                transforms.ToTensor(),
-                #transforms.Normalize((0.1307,), (0.3081,))
-            ]),
+            transform=transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    # transforms.Normalize((0.1307,), (0.3081,))
+                ]
+            ),
         )
 
         return train_dataset, test_dataset
--- mnist/make_cluster_dataset.py
+++ mnist/make_cluster_dataset.py
@@ -16,7 +16,9 @@ def get_bottleneck_features(dataloader, model, classes, num_samples_per_class):
     with torch.no_grad():
         for data, target in dataloader:
             # Check if we have enough samples for each class
-            if all(count >= num_samples_per_class for count in class_samples_count.values()):
+            if all(
+                count >= num_samples_per_class for count in class_samples_count.values()
+            ):
                 break
 
             data = data.flatten(1, -1).to(device)
@@ -25,7 +27,10 @@ def get_bottleneck_features(dataloader, model, classes, num_samples_per_class):
             h = h.detach().cpu()
 
             for feature, label in zip(h, target):
-                if label.item() in class_samples_count and class_samples_count[label.item()] < num_samples_per_class:
+                if (
+                    label.item() in class_samples_count
+                    and class_samples_count[label.item()] < num_samples_per_class
+                ):
                     bottleneck_features.append(feature.numpy())
                     labels.append(label.item())
                     class_samples_count[label.item()] += 1
@@ -33,29 +38,30 @@ def get_bottleneck_features(dataloader, model, classes, num_samples_per_class):
     return np.array(bottleneck_features), np.array(labels)
 
 
-dataset             = 'mnist'
-data_dir            = '/mnt/datasets/MNIST'
-save_dir            = './out'
-batch_size          = 128
-model_weights_path  = './out/mnist_ae_ep=1000.pt'
-num_workers         = 4
-device              = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-num_samples         = 100  # number of samples per class to save
-classes             = (0, 1, 2, 3)  # classes to save
+dataset = "mnist"
+data_dir = "./datasets/MNIST"
+save_dir = "./out"
+batch_size = 128
+model_weights_path = "./out/mnist_ae_ep=1000.pt"
+num_workers = 1
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_samples = 100  # number of samples per class to save
+classes = (0, 1, 2)  # classes to save
 
 # Load the model
 model = Autoencoder()
-model.load_state_dict(torch.load(model_weights_path))
+model.load_state_dict(torch.load(model_weights_path, map_location=device))
 model.to(device)
 model.eval()
 
 # DataLoader
 vision_dataset = VisionDataset(name=dataset, root=data_dir)
-train_loader, _ = vision_dataset.get_dataloaders(batch_size=batch_size, num_workers=num_workers)
+train_loader, _ = vision_dataset.get_dataloaders(
+    batch_size=batch_size, num_workers=num_workers
+)
 
 features, labels = get_bottleneck_features(train_loader, model, classes, num_samples)
 
 # Save the dataset
-np.save(f'{save_dir}/bottleneck_features.npy', features)
-np.save(f'{save_dir}/labels.npy', labels)
-
+np.save(f"{save_dir}/bottleneck_features.npy", features)
+np.save(f"{save_dir}/labels.npy", labels)
